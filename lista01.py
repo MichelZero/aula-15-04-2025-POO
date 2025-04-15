082fab94ef5e754e8a69d4372f0fe412623a19bd
Michel <106674127+MichelZero@users.noreply.github.com>
2025-04-15 11:22:30 -0300

    up

---
+++ lista01.py
@@ -0,0 +1,13 @@
+#
+#
+#
+
+L = [0, 1, [2, 3, [4, 5]], 6, 7]
+print("print o 0")
+print(L[0])
+print("print o 6")
+print(L[-2])
+print("print o 3")
+print(L[2][1])
+print("print o 5")
+print(L[2][2][1])
